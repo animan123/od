55a0f9307f77302e2893c88ce5960315e779d84d
animan123 <anirudh.1203@yahoo.in>
2016-12-22 21:39:22 +0530

    Piece defined to be unsigned 64 bit int

--- src/constants.h
+++ src/constants.h
@@ -1,8 +1,14 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include <cstdint>
+
+//color based indexing
 #define WHITE 0
 #define BLACK 1
 #define ALL 2
+
+//Each bitboard is an unsigned 64 bit integer
+#define piece uint_64_t
 
 #endif
