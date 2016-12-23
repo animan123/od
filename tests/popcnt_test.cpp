c56243d5096bc2a855449f9bf0e247c33977555d
animan123 <anirudh.1203@yahoo.in>
2016-12-23 13:41:06 +0530

    Tests for POPMSB

--- tests/popcnt_test.cpp
+++ tests/popcnt_test.cpp
@@ -12,9 +12,22 @@ int main () {
 
 	int counter = 0;
 	while (x) {
-		int i = POPCNT(x);
-		test(i==counter, "Pop count test for " + to_string(counter));
+		int i = POPLSB(x);
+		test (i==counter, "Pop LSB test for " + to_string(counter));
 		counter += 1;
 		RESET(x, i);
 	}
+
+	for(int i=0; i<64; ++i) {
+		SET(x, i);
+	}
+
+	counter=63;
+	while (x) {
+		int i = POPMSB(x);
+		test (i==counter, "Pop MSB test for " + to_string(counter));
+		counter -= 1;
+		RESET(x, i);
+	}
+	return 0;
 }
