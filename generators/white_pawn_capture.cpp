b9613a9be75f2c666dd92c7009e3c7d609b53a1e
animan123 <anirudh.1203@yahoo.in>
2016-12-27 17:29:05 +0530

    generator for white pawn capture

---
+++ generators/white_pawn_capture.cpp
@@ -0,0 +1,49 @@
+/*
+	Generate bitboard for white pawn capture moves
+	for a particular square
+*/
+
+#include "../src/constants.h"
+#include "../src/utils.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int within_bounds (char first, char second) {
+	if (first < 'A' || first > 'H' || second < '1' || second > '8') {
+		return 0;
+	}
+	return 1;
+}
+
+int main () {
+	bitboard white_pawn_capture[64];
+	for(char first='A'; first<='H'; ++first) {
+		for(char second='1'; second<='8'; ++second) {
+			string notation(2, 'x');
+			notation[0] = first;
+			notation[1] = second;
+			int index = INDEX (notation);
+			white_pawn_capture[index] = BITBOARD (0);
+			if (second == '1') {
+				continue;
+			}
+			if (within_bounds (first-1, second+1)) {
+				notation[0] = first - 1;
+				notation[1] = second + 1;
+				SET_NOTATION (white_pawn_capture[index], notation);
+			}
+			if (within_bounds (first+1, second+1)) {
+				notation[0] = first + 1;
+				notation[1] = second + 1;
+				SET_NOTATION (white_pawn_capture[index], notation);
+			}
+		}
+	}
+	cout << "static const bitboard white_pawn_capture_moves[64] = {" << endl;
+	for (int i=0; i<64; ++i) {
+		cout << "BITBOARD(" << white_pawn_capture[i] << "), ";
+	}
+	cout << "};" << endl;
+	return 0;
+}
