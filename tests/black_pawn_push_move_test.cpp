8f60f5c9cad50108d347bc1e237c5f6b4f975d06
animan123 <anirudh.1203@yahoo.in>
2016-12-27 16:58:40 +0530

    Added bitboards for pawn push moves and unit tests

---
+++ tests/black_pawn_push_move_test.cpp
@@ -0,0 +1,16 @@
+#include "../src/constants.h"
+#include "../src/moves.h"
+#include "../src/utils.h"
+#include "tests.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main () {
+	PRINTBOARD (black_pawn_push_moves[INDEX("A1")], "black_pawn_push moves for A1");
+	PRINTBOARD (black_pawn_push_moves[INDEX("A7")], "black_pawn_push moves for A7");
+	PRINTBOARD (black_pawn_push_moves[INDEX("D7")], "black_pawn_push moves for D7");
+	PRINTBOARD (black_pawn_push_moves[INDEX("H2")], "black_pawn_push moves for H2");
+	PRINTBOARD (black_pawn_push_moves[INDEX("B8")], "black_pawn_push moves for B8");
+	return 0;
+}
---
+++ tests/white_pawn_push_move_test.cpp
@@ -0,0 +1,16 @@
+#include "../src/constants.h"
+#include "../src/moves.h"
+#include "../src/utils.h"
+#include "tests.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main () {
+	PRINTBOARD (white_pawn_push_moves[INDEX("A1")], "white_pawn_push moves for A1");
+	PRINTBOARD (white_pawn_push_moves[INDEX("A2")], "white_pawn_push moves for A2");
+	PRINTBOARD (white_pawn_push_moves[INDEX("E2")], "white_pawn_push moves for E2");
+	PRINTBOARD (white_pawn_push_moves[INDEX("H7")], "white_pawn_push moves for H7");
+	PRINTBOARD (white_pawn_push_moves[INDEX("B8")], "white_pawn_push moves for B8");
+	return 0;
+}
