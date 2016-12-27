f333c18a3db10d215a299b24ab3b8de267c49eed
animan123 <anirudh.1203@yahoo.in>
2016-12-27 14:50:07 +0530

    added knight move constants to move and unit tests'

---
+++ tests/knight_moves_test.cpp
@@ -0,0 +1,18 @@
+#include "../src/constants.h"
+#include "../src/moves.h"
+#include "../src/utils.h"
+#include "tests.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main () {
+	PRINTBOARD (knight_moves[INDEX("A1")], "Knight moves for A1");
+	PRINTBOARD (knight_moves[INDEX("H8")], "Knight moves for H8");
+	PRINTBOARD (knight_moves[INDEX("A8")], "Knight moves for A8");
+	PRINTBOARD (knight_moves[INDEX("H1")], "Knight moves for H1");
+	PRINTBOARD (knight_moves[INDEX("E4")], "Knight moves for E4");
+	PRINTBOARD (knight_moves[INDEX("E1")], "Knight moves for E1");
+	PRINTBOARD (knight_moves[INDEX("E8")], "Knight moves for E8");
+return 0;
+}
