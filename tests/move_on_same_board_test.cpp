86dcb3b5ee3f866b0009992b9ac8f72b871a02df
animan123 <anirudh.1203@yahoo.in>
2016-12-28 19:32:46 +0530

    Unit test (need more) for move on same board

---
+++ tests/move_on_same_board_test.cpp
@@ -0,0 +1,13 @@
+#include "../src/moves.h"
+#include "../src/constants.h"
+#include "../src/utils.h"
+#include "../src/board.h"
+#include "tests.h"
+
+int main () {
+	board b;
+	MOVE_ON_SAME_BOARD(b, INDEX("G1"), INDEX("F3"));
+	PRINTBOARD (b.all[ALL], "G2 to F3 on start board all");
+	PRINTBOARD (b.knight[ALL], "G2 to F3 on start board all");
+	return 0;
+}
