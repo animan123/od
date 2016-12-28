39c08c227639dce3740b3217950f6ab3ff5ddc9b
animan123 <anirudh.1203@yahoo.in>
2016-12-28 12:45:38 +0530

    macrofied unit tests for color and piece

--- tests/board_color_piece_test.cpp
+++ tests/board_color_piece_test.cpp
@@ -2,21 +2,22 @@
 #include "../src/constants.h"
 #include "../src/utils.h"
 #include "tests.h"
+#include <iostream>
+using namespace std;
 
 int main () {
 	board b;
-	TEST(b.square_color(INDEX("A1")) == WHITE, "color of A1 test");
-	TEST(b.square_color(INDEX("A8")) == BLACK, "color of A8 test");
-	TEST(b.square_color(INDEX("E4")) == NOCOLOR, "color of E4 test");
-	TEST(b.square_color(INDEX("D5")) == NOCOLOR, "color of D5 test");
-	TEST(b.square_color(INDEX("H2")) == WHITE, "color of H2 test");
+	TEST(COLOR_OF_PIECE_ON(b, INDEX("A1")) == WHITE, "color of A1 test");
+	TEST(COLOR_OF_PIECE_ON(b, INDEX("A8")) == BLACK, "color of A8 test");
+	TEST(COLOR_OF_PIECE_ON(b, INDEX("E4")) == NOCOLOR, "color of E4 test");
+	TEST(COLOR_OF_PIECE_ON(b, INDEX("D5")) == NOCOLOR, "color of D5 test");
+	TEST(COLOR_OF_PIECE_ON(b, INDEX("H2")) == WHITE, "color of H2 test");
 
-	TEST(b.square_piece(INDEX("A1")) == ROOK, "piece test of A1");
-	TEST(b.square_piece(INDEX("B8")) == KNIGHT, "piece test of B8");
-	TEST(b.square_piece(INDEX("D1")) == QUEEN, "piece test of D1");
-	TEST(b.square_piece(INDEX("E8")) == KING, "piece test of E8");
-	TEST(b.square_piece(INDEX("C4")) == NOPIECE, "piece test of C4");
-
+	TEST(PIECE_ON(b, INDEX("A1")) == ROOK, "piece test of A1");
+	TEST(PIECE_ON(b, INDEX("B8")) == KNIGHT, "piece test of B8");
+	TEST(PIECE_ON(b, INDEX("D1")) == QUEEN, "piece test of D1");
+	TEST(PIECE_ON(b, INDEX("E8")) == KING, "piece test of E8");
+	TEST(PIECE_ON(b, INDEX("C4")) == NOPIECE, "piece test of C4");
 	return 0;
 }
 
