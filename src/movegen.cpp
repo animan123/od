f4988fe873b0e25563bc7e3606c8b854ef9ecd61
animan123 <anirudh.1203@yahoo.in>
2017-01-03 18:51:13 +0530

    King move generator(without castling)

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -7,7 +7,8 @@
 
 MoveList gen_moves (board b, int color) {
 	MoveList move_list;
-	gen_knight_moves (move_list, b, color);
+	//gen_knight_moves (move_list, b, color);
+	gen_king_moves (move_list, b, color);
 	return move_list;
 }
 
@@ -21,6 +22,20 @@ void gen_knight_moves (MoveList &move_list, board b, int color) {
 		while (knight_move) {
 			int to = POPLSB_AND_RESET (knight_move);
 			move_list.push  (Move(from, to));
+		}
+	}
+}
+
+void gen_king_moves (MoveList &move_list, board b, int color) {
+	bitboard king_board = b.king[color];
+	int from, to;
+	bitboard king_move;
+	while (king_board) {
+		from = POPLSB_AND_RESET (king_board);
+		king_move = (king_moves[from] & (~b.all[color]));
+		while (king_move) {
+			to = POPLSB_AND_RESET (king_move);
+			move_list.push (Move(from, to));
 		}
 	}
 }
