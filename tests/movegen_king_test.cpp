dfc7595987e686db75dda96bc1c2c0585c2977fc
animan123 <anirudh.1203@yahoo.in>
2017-01-03 18:52:09 +0530

    Tests for king move gen

---
+++ tests/movegen_king_test.cpp
@@ -0,0 +1,36 @@
+#include "../src/moves.h"
+#include "../src/constants.h"
+#include "../src/utils.h"
+#include "../src/board.h"
+#include "../src/movegen.h"
+#include "tests.h"
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int main () {
+	board b;
+	MOVE_ON_SAME_BOARD (b, INDEX("E2"), INDEX("E4"));
+	MOVE_ON_SAME_BOARD (b, INDEX("E7"), INDEX("E5"));
+	MOVE_ON_SAME_BOARD (b, INDEX("G1"), INDEX("F3"));
+	MOVE_ON_SAME_BOARD (b, INDEX("D7"), INDEX("D5"));
+	MOVE_ON_SAME_BOARD (b, INDEX("F1"), INDEX("C4"));
+	PRINTBOARD (b.all[ALL], "Originally .. ");
+	MoveList move_list;
+	gen_king_moves (move_list, b, WHITE);
+	while (!move_list.empty()) {
+		Move m = move_list.top ();
+		move_list.pop ();
+		if MOVE_IS_CAPTURE (b, m.from, m.to) {
+			board c = MOVE_ON_DIFFERENT_BOARD (b, m.from, m.to);
+			PRINTBOARD (c.all[ALL], m.move_name());
+		} else {
+			MOVE_ON_SAME_BOARD (b, m.from, m.to);
+			PRINTBOARD (b.all[ALL], m.move_name());
+			UNMOVE_ON_SAME_BOARD (b, m.from, m.to);
+		}
+		int input;
+		cin>>input;
+	}
+	return 0;
+}
